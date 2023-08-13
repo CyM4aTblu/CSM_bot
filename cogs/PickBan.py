65789f479c738f2bfeb43cda501122aeeb3b68f0
CyM4aTblu <dimamik34@gmail.com>
2023-08-13 15:24:03 +0300

    completed team verification

--- cogs/PickBan.py
+++ cogs/PickBan.py
@@ -5,7 +5,7 @@ from discord import app_commands
 
 
 class Team:
-    def __init__(self, name, seed, tag):
+    def __init__(self, name, seed, tag: discord.Role):
         self.name = name
         self.seed = seed
         self.tag = tag
@@ -21,11 +21,11 @@ class MapBanButton(discord.ui.Button):
         self.disabled = True
         self.style = discord.ButtonStyle.grey
         if self.view.bannedMapsCounter == 6:
-            map_choosed_embed = discord.Embed(
-                title=f"***ИМЯ КОМАНДЫ С НИЗКИМ СИДОМ*** выбрали карту:  ***{self.view.bannedMapsNumbers}***",
+            map_choosed_embed = discord.Embed(title=f"Карта  ***{self.view.bannedMapsNumbers}***  была выбрана командой:",
+                description=f"**<@&{self.view.outsiders.id}>**",
                 color= discord.Color.dark_teal()
             )
-            await interaction.response.send_message(embed=map_choosed_embed)
+            await interaction.response.send_message(embed=map_choosed_embed) #FIXME: баг не обновления статуса кнопки (невозможность использовать множество interaction.response)
             self.view.stop()
         elif self.view.bannedMapsCounter == 2 or self.view.bannedMapsCounter == 5:
             maps_banned_embed = discord.Embed(
@@ -37,8 +37,10 @@ class MapBanButton(discord.ui.Button):
 
 
 class MapsView(discord.ui.View):
-    def __init__(self):
+    def __init__(self, outsiders, frontRunners):
         super().__init__()
+        self.outsiders = outsiders
+        self.frontRunners = frontRunners
         self.add_item(MapBanButton("1", 1))
         self.add_item(MapBanButton("2", 1))
         self.add_item(MapBanButton("3", 1))
@@ -65,13 +67,13 @@ class ModeBanButton(discord.ui.Button):
 
     async def callback(self, interaction: discord.Interaction):
         if self.view.bannedMode != "" and self.view.bannedMode != self.label:
-            mapView = MapsView()
+            mapView = MapsView(self.view.outsiders, self.view.frontRunners)
             mapView.mode = self.label
             choose_embed = discord.Embed(title=f"{interaction.user.name} Выбрал режим:  ***{mapView.mode}***",
-                                         description="***ИМЯ КОМАНДЫ С НИЗКИМ СИДОМ*** должна забанить 2 из 8"
+                                         description=f"<@&{self.view.outsiders.id}> должна забанить 2 из 8"
                                                      " карт нажав на сответствующие кнопки\n"
-                                                     "Далее ***ИМЯ КОМАНДЫ С ВЫСОКИМ СИДОМ*** банит 3 карты из оставшихся\n"
-                                                     "После чего ***ИМЯ КОМАНДЫ С НИЗКИМ СИДОМ*** выбирает карту из 3-х оставшихся",
+                                                     f"Далее <@&{self.view.frontRunners.id}> банит 3 карты из оставшихся\n"
+                                                     f"После чего <@&{self.view.outsiders.id}> выбирает карту из 3-х оставшихся",
                                          colour=discord.Colour.blurple())
             choose_embed.set_image(url=mapView.imageUrls[mapView.mode])
             await interaction.response.send_message(embed=choose_embed, view=mapView)
@@ -87,8 +89,10 @@ class ModeBanButton(discord.ui.Button):
 
 
 class ModesView(discord.ui.View):
-    def __init__(self):
+    def __init__(self, outsiders, frontRunners):
         super().__init__()
+        self.outsiders = outsiders
+        self.frontRunners = frontRunners
         self.add_item(ModeBanButton("Мегакарп", discord.ButtonStyle.primary))
         self.add_item(ModeBanButton("Бой за Башню", discord.ButtonStyle.green))
         self.add_item(ModeBanButton("Устробол", discord.ButtonStyle.secondary))
@@ -100,19 +104,33 @@ class PickBan(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    Teams = [
+        Team("Sync", 1, 2),
+        Team("Чай", 2, 3),
+        Team("Floyd", 3, 4)
+    ]
+
     @commands.Cog.listener()
     async def on_ready(self):
         print("PickBan - online")
 
     @app_commands.command(name="start", description="Начать процесс выбора карты")
-    @app_commands.describe(Team_1="Дискорд-тэг твоей команды", Team_2="Дискорд-тэг команды противника")
-    async def start(self, interaction: discord.Interaction, Team_1: discord.Role, Team_2: discord.Role):
-        modeView = ModesView()
-        print(Team_1)
+    @app_commands.describe(team1="Дискорд-тэг твоей команды", team2="Дискорд-тэг команды противника")
+    async def start(self, interaction: discord.Interaction, team1: discord.Role, team2: discord.Role): #TODO: красивое название параметров вввода
+        for i in self.Teams:
+            if i.name == team1.name:
+                outsiders = team1
+                seedOut = i.seed
+            elif i.name == team2.name:
+                frontRunners = team2
+                seedRnrs = i.seed
+        if seedOut > seedRnrs:
+            outsiders, frontRunners = frontRunners, outsiders
+        modeView = ModesView(outsiders, frontRunners)
         modes_embed = discord.Embed(title="Этап №1 - Выбор режима",
-                                    description="***ИМЯ КОМАНДЫ С ВЫСОКИМ СИДОМ*** должна забанить 1 из 4"
+                                    description=f"<@&{frontRunners.id}> должна забанить 1 из 4"
                                                 " режимов нажав на сответствующую кнопку\n\n\n"
-                                                "После чего ***ИМЯ КОМАНДЫ С НИЗКИМ СИДОМ*** выбирает режим из 3-х оставшихся",
+                                                f"После чего <@&{outsiders.id}> выбирает режим из 3-х оставшихся",
                                     colour=discord.Colour.gold())
         modes_embed.set_image(url=
                               "https://media.discordapp.net/attachments/994356082313023560/1139622287717437600/FQ_v1XwXMAA-v8G.png?width=972&height=670")
